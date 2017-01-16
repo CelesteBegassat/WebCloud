fcbefff290a350b5ff8b2e94956dfbe7ce1e9b73
Nicolas <nico2che@gmail.com>
2017-01-16 12:32:29 +0100

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+tail -n +3 "$0" | ssh ubuntu@35.157.32.132; exit;
+
+# Prevent errors
+set -e
+
+# Send apt-get update, upgrade, install ngnix
+echo " ... apt-get update..."
+sudo apt-get update
+echo " ... apt-get upgrade..."
+sudo apt-get upgrade
+echo " ... apt-get install nginx..."
+sudo apt-get install nginx
+
+# Set permission to html folder
+# chown ubuntu:ubuntu .
+cd /var/www/html
+sudo git clone https://github.com/CelesteBegassat/WebCloud.git
