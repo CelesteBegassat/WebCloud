c0764f5a4c987ac0643460be6c052fc6a9717cf9
Nicolas <nico2che@gmail.com>
2017-01-20 11:34:54 +0100

    Deploy, add some conditions

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,55 @@
 #!/bin/bash
-tail -n +3 "$0" | ssh ubuntu@35.157.32.132; exit;
+if [ "$1" = '' ]; then
+    echo "Quelle est l'adresse IP de votre serveur ? [35.157.32.132] : "
+    read IP
+    if [ "$IP" = '' ]; then
+        IP="35.157.32.132"
+    fi
+else
+    IP=$1
+fi
+if [ "$2" = '' ]; then
+    echo "Avec quel utilisateur voulez-vous vous connecter ? [ubuntu] : "
+    read USER
+    if [ "$USER" = '' ]; then
+        USER="ubuntu"
+    fi
+else
+    USER=$2
+fi
+tail -n +22 "$0" | ssh $USER@$IP; exit;
 
 # Prevent errors
 set -e
 
+echo "Vous êtes sur une machine $(uname)"
+
 # Send apt-get update, upgrade, install ngnix
-echo " ... apt-get update..."
-sudo apt-get update
-echo " ... apt-get upgrade..."
-sudo apt-get upgrade -y
-echo " ... apt-get install nginx..."
-sudo apt-get install nginx -y
+echo " ### apt-get update"
+sudo apt-get update > ~/latest-update.log
+echo " ### apt-get upgrade"
+sudo apt-get upgrade -y > ~/latest-upgrade.log
 
-# Set permission to html folder
-# chown ubuntu:ubuntu .
-cd /var/www/html
+# Installation du serveur web
+echo " ### apt-get install nginx"
+sudo apt-get install nginx  -y 2>> ~/error-installs.log > /dev/null
 
-# TODO: pull if exist, please
-sudo rm -Rf WebCloud
-sudo git clone https://github.com/CelesteBegassat/WebCloud.git
+# Installation d'outils utiles, htop et git
+echo " ### apt-get install htop git"
+sudo apt-get install htop git  -y 2>> ~/error-installs.log > /dev/null
+
+cd /var/www
+
+if [ -d html/.git ]; then
+    echo " ### Un projet semble initialisé"
+    echo " ### Mise à jour du projet ... "
+    cd html
+    git pull
+else
+    echo " ## Le projet n'a jamais été déployé pour le moment"
+    echo " ## Mise en place du projet ... "
+    sudo chown -Rf $USER:$USER .
+    rm -Rf html
+    git clone https://github.com/CelesteBegassat/WebCloud.git html
+fi
+
